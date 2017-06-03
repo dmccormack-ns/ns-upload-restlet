ca4df14ce5ef2c72868362d2ade48116ede14c62
dmccormack34 <dmccormack34@gmail.com>
2017-06-03 12:10:04 -0500

    init project

---
+++ src/ns_uploader.ts
@@ -0,0 +1,13 @@
+/**
+ * @NApiVersion 2.x
+ * @NModuleScope Public
+ * @NScriptType RESTLet
+ */
+
+import * as log from 'N/log';
+import {EntryPoints} from 'N/Types';
+
+export function post () {
+    return false;
+}
+
